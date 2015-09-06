b1f035a889af6fde4257a809f28d57426937fa0c
Ben Haaland <bphaaland@gmail.com>
2015-09-06 18:52:44 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$( document ).ready(function(){
+    $(".button-collapse").sideNav();
+    $('.parallax').parallax();
+    $('ul.tabs').tabs();
+});
+
+// Owl.Carousel.js plug-in initialization function.
+$(document).ready(function() {
+    $("#owl-example").owlCarousel({
+        singleItem: true,
+        autoPlay: 3500,
+        stopOnHover: true
+    });
+});
+
+
+
+
